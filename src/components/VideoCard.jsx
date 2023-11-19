9f8c1e52c4a371e087308bc7a42586fc493124d6
Maxim Litvinov <maximlitvinov.2611@gmail.com>
2023-11-20 01:23:12 +0300

    work with videoDetailComponent

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -17,38 +17,48 @@ const VideoCard = ({
   },
 }) => {
   return (
-    <Card sx={{
-      width:{md: '320px', xs:'100%'},
-      boxShadow: 'none',
-      borderRadius:4,
-      backgroundColor: '#1e1e1e'
-    }}>
+    <Card
+      sx={{
+        width: { xs: "100%", sm: "358px", md: "320px" },
+        boxShadow: "none",
+        borderRadius: 4,
+        backgroundColor: "#1e1e1e",
+      }}
+    >
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
         <CardMedia
           component="img"
           image={snippet?.thumbnails?.high?.url}
           alt={snippet?.title}
           sx={{
-            width: 360,
-            height: 200,
+            width: { xs: "100%", sm: "358px", md: "320px" },
+            height: 180,
           }}
         />
       </Link>
       <CardContent sx={{ backgroundColor: "#1e1e1e", height: "106px" }}>
         <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
-          <Typography variant="subtitle1" fontWeight='bold' color='#fff'>
+          <Typography variant="subtitle1" fontWeight="bold" color="#fff">
             {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
           </Typography>
         </Link>
 
-        <Link to={snippet?.channelId ? `/channel/${snippet.channelId}` : demoChannelUrl}>
-          <Typography variant="subtitle2" fontWeight='bold' color='gray'>
+        <Link
+          to={
+            snippet?.channelId
+              ? `/channel/${snippet.channelId}`
+              : demoChannelUrl
+          }
+        >
+          <Typography variant="subtitle2" fontWeight="bold" color="gray">
             {snippet?.channelTitle || demoChannelTitle}
-            <CheckCircle sx={{
-              fontSize: 12,
-              color: 'gray',
-              ml: '5px'
-            }}/>
+            <CheckCircle
+              sx={{
+                fontSize: 12,
+                color: "gray",
+                ml: "5px",
+              }}
+            />
           </Typography>
         </Link>
       </CardContent>
--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,9 +1,115 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import ReactPlayer from "react-player";
+import { Typography, Box, Stack } from "@mui/material";
+
+import { CheckCircle } from "@mui/icons-material";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import ThumbUpIcon from "@mui/icons-material/ThumbUp";
+
+import { Videos } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const VideoDetail = () => {
-  return (
-    <div>VideoDetail</div>
-  )
-}
+  const { id } = useParams();
+  const [videoDetail, setVideoDetail] = useState(null);
+  const [videos, setVideos] = useState([]);
 
-export default VideoDetail
+  useEffect(() => {
+    fetchFromAPI(`videos?part=snippet,statistics&id=${id}`).then((data) =>
+      setVideoDetail(data.items[0])
+    );
+
+    fetchFromAPI(`search?part=snippet&relatedToVideoId=${id}&type=video`).then(
+      (data) => setVideos(data.items)
+    );
+  }, [id]);
+
+  if (!videoDetail?.snippet) return "Loading...";
+
+  const {
+    snippet: { title, channelId, channelTitle },
+    statistics: { viewCount, likeCount },
+  } = videoDetail;
+
+  return (
+    <Box minHeight="95vh">
+      <Stack direction={{ xs: "column", md: "row" }}>
+        <Box flex={1}>
+          <Box sx={{ width: "100%", position: "sticky", top: "86px" }}>
+            <ReactPlayer
+              url={`https://www.youtube.com/watch?v=${id}`}
+              className="react-player"
+              controls
+            />
+            <Typography color="#fff" m={2} variant="h5" fontWeight="bold">
+              {title}
+            </Typography>
+            <Stack
+              direction="row"
+              justifyContent="space-between"
+              sx={{ color: "#fff" }}
+              px={2}
+            >
+              <Link to={`/channel/${channelId}`}>
+                <Typography
+                  variant={{ md: "h6", sm: "subtitle1" }}
+                  color="#fff"
+                >
+                  {channelTitle}
+                  <CheckCircle
+                    sx={{
+                      color: "gray",
+                      fontSize: "12px",
+                      marginLeft: "5px",
+                    }}
+                  />
+                </Typography>
+              </Link>
+              <Stack
+                direction="row"
+                gap="15px"
+                alignItems="center"
+                justifyContent="space-between"
+              >
+                <VisibilityIcon
+                  sx={{
+                    color: "gray",
+                    fontSize: "17px",
+                  }}
+                />
+                <Typography variant="body1" sx={{ opacity: 0.7 }}>
+                  {parseInt(viewCount).toLocaleString()} views
+                </Typography>
+
+                <ThumbUpIcon
+                  sx={{
+                    marginLeft: "10px",
+                    color: "gray",
+                    fontSize: "17px",
+                  }}
+                />
+                <Typography
+                  variant="body1"
+                  sx={{ opacity: 0.7, marginLeft: "5px" }}
+                >
+                  {parseInt(likeCount).toLocaleString()} likes
+                </Typography>
+              </Stack>
+            </Stack>
+          </Box>
+        </Box>
+        <Box
+          px={2}
+          py={{ md: 1, xs: 5 }}
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Videos videos={videos} direction="column" />
+        </Box>
+      </Stack>
+    </Box>
+  );
+};
+
+export default VideoDetail;
